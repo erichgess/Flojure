b00ecf296fdb327dc9501dca02d53bb520057c08
Erich Ess <erich.g.ess@gmail.com>
2014-02-17 11:40:21 -0800

    First commit of a program which will convert F# to Clojure.

---
+++ Flojure/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+
+let rec ConvertToClojure expr =
+    match expr with
+    | Call(o, m, ps ) ->
+        printf "( %s " m.Name
+        for e in ps do
+            ConvertToClojure e
+        printf " )"
+    | Value(v,ty) -> printf " %A " v
+    | _ -> printf "unknown"
+
+[<EntryPoint>]
+let main argv = 
+    let test = <@ 2 + 2 * 3 @>
+    ConvertToClojure test
+    printfn "\n\ndone"
+    0 // return an integer exit code
