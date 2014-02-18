7fc4f08eef4b8c048fdb3c9e3b462c433e34390e
Erich Ess <erich.g.ess@gmail.com>
2014-02-18 12:43:28 -0800

    Refactored the main Flojure project by moving the ConvertToClojure method into its own module.
    Added some unit tests, which don't work because Flojure just prints to stdio instead of building a string.

--- Flojure/Program.fs
+++ Flojure/Program.fs
@@ -1,59 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
-open Microsoft.FSharp.Quotations
-open Microsoft.FSharp.Quotations.Patterns
-
-let ConvertToClojure expr =
-    let rec ConvertToClojure expr =
-        match expr with
-        | Call(o, m, ps ) ->
-            printf "( %s " m.Name
-            for e in ps do
-                ConvertToClojure e
-            printf ") "
-        | Value(v,ty) -> printf " %A " v
-        | Let(var, definition, useIn) ->
-            printf "( def %s " var.Name
-            ConvertToClojure definition
-            printf " )\n"
-            ConvertToClojure useIn
-        | Application(func, expr) -> 
-            printf "( "
-            ConvertToClojure func
-            ConvertToClojure expr
-            printf ") "
-        | Lambda(var1, e) ->
-            printf "( fn "
-            printf "[%s] " var1.Name
-            ConvertToClojure e
-            printf ") "
-        | Var(v) -> printf " %s " v.Name
-        | IfThenElse(clause, thenExpr, elseExpr ) ->
-            printf "(if "
-            ConvertToClojure clause
-            ConvertToClojure thenExpr
-            ConvertToClojure elseExpr
-            printf ") "
-        | NewUnionCase(info, expr ) ->
-            printf "( "
-            match info.Name with
-            | "Cons" -> 
-                printf "cons "
-                expr |> List.iter ( fun e -> ConvertToClojure e )
-            | "Empty" -> printf "list"
-            | _ -> failwithf "Unrecognized UnionCase info type %A" info.Name
-            printf ") "
-        | ForIntegerRangeLoop( var, start, finish, body ) ->
-            printf  "(for [%s ( range " var.Name
-            ConvertToClojure start
-            ConvertToClojure (<@ %%finish + 1 @>)
-            printf ")] "
-            ConvertToClojure body
-            printf ") "
-        | _ -> printf "unknown"
-    ConvertToClojure expr
-    printfn "\n\n"
+open ConvertToClojure
 
 [<EntryPoint>]
 let main argv = 
