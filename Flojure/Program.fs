fa2b0e66ddacf37644a335533aee1d4144d21a72
Erich Ess <erich.g.ess@gmail.com>
2014-02-17 22:43:06 -0800

    Added a more complex test which builds a list by calling functions to perform calculations.

--- Flojure/Program.fs
+++ Flojure/Program.fs
@@ -4,40 +4,43 @@
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Quotations.Patterns
 
-let rec ConvertToClojure expr =
-    match expr with
-    | Call(o, m, ps ) ->
-        printf "( %s " m.Name
-        for e in ps do
+let ConvertToClojure expr =
+    let rec ConvertToClojure expr =
+        match expr with
+        | Call(o, m, ps ) ->
+            printf "( %s " m.Name
+            for e in ps do
+                ConvertToClojure e
+            printf ") "
+        | Value(v,ty) -> printf " %A " v
+        | Let(var, definition, useIn) ->
+            printf "( def %s " var.Name
+            ConvertToClojure definition
+            printf " )\n"
+            ConvertToClojure useIn
+        | Application(func, expr) -> 
+            printf "( "
+            ConvertToClojure func
+            ConvertToClojure expr
+            printf ") "
+        | Lambda(var1, e) ->
+            printf "( fn "
+            printf "[%s] " var1.Name
             ConvertToClojure e
-        printf ") "
-    | Value(v,ty) -> printf " %A " v
-    | Let(var, definition, useIn) ->
-        printf "( def %s " var.Name
-        ConvertToClojure definition
-        printf " )\n"
-        ConvertToClojure useIn
-    | Application(func, expr) -> 
-        printf "( "
-        ConvertToClojure func
-        ConvertToClojure expr
-        printf ") "
-    | Lambda(var1, e) ->
-        printf "( fn "
-        printf "[%s] " var1.Name
-        ConvertToClojure e
-        printf ") "
-    | Var(v) -> printf " %s " v.Name
-    | NewUnionCase(info, expr ) ->
-        printf "( "
-        match info.Name with
-        | "Cons" -> 
-            printf "cons "
-            expr |> List.iter ( fun e -> ConvertToClojure e )
-        | "Empty" -> printf "list"
-        | _ -> failwithf "Unrecognized UnionCase info type %A" info.Name
-        printf ") "
-    | _ -> printf "unknown"
+            printf ") "
+        | Var(v) -> printf " %s " v.Name
+        | NewUnionCase(info, expr ) ->
+            printf "( "
+            match info.Name with
+            | "Cons" -> 
+                printf "cons "
+                expr |> List.iter ( fun e -> ConvertToClojure e )
+            | "Empty" -> printf "list"
+            | _ -> failwithf "Unrecognized UnionCase info type %A" info.Name
+            printf ") "
+        | _ -> printf "unknown"
+    ConvertToClojure expr
+    printfn "\n"
 
 [<EntryPoint>]
 let main argv = 
@@ -46,9 +49,12 @@ let main argv =
     printfn "\n\ndone"
 
     printfn "\n\nTest Lists"
-    let test = <@ [1; 2; 3 + 3] @>
+    let test = <@ [1; 2; 3 + 3; 5; 6] @>
     ConvertToClojure test
     
+
+    let test = <@ let x y z = y + z * 3 in let q = 3 in [1; 2; x 3 3; 5; 6; q] @>
+    ConvertToClojure test
 
     printfn "\n\n"
     0 // return an integer exit code
