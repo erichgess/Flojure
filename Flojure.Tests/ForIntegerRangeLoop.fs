99bc5f45a939418fc8508c35c582dbf68c768fdd
Erich Ess <erich.g.ess@gmail.com>
2014-02-18 12:45:18 -0800

    Forgot to add the new files to the repo.

---
+++ Flojure.Tests/ForIntegerRangeLoop.fs
@@ -0,0 +1,19 @@
+﻿module ForIntegerRangeLoop
+
+open NUnit.Framework
+open FsUnit
+open ConvertToClojure
+
+
+[<TestFixture>] 
+type ``Given a a for loop on an integer range`` () =
+    
+    [<Test>] member this.
+     ``when using "for i in 1 .. 2 do", it returns a Clojure for`` () =
+        ConvertToClojure <@ for i = 1 to 10 do printfn "test" @> 
+        |> should equal "(for [i ( range  1 ( op_Addition  10  1 ) )] ( PrintFormatLine unknown) )"
+
+    [<Test>] member this.
+     ``when using an expression as the finish clause of the foor loop, it returns a Clojure for`` () =
+        ConvertToClojure <@ let finish = 10 in for i = 1 to (finish + 3) do printfn "test" @> 
+        |> should equal "(for [i ( range  1 ( op_Addition  10  1 ) )] ( PrintFormatLine unknown) )"
