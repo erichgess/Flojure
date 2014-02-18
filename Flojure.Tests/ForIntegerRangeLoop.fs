49ce5356707ed847674acc0950b028cc36b7a0fe
Erich Ess <erich.g.ess@gmail.com>
2014-02-18 13:28:24 -0800

    Added a more complex for loop test: this one has an if expression in the start clause.

--- Flojure.Tests/ForIntegerRangeLoop.fs
+++ Flojure.Tests/ForIntegerRangeLoop.fs
@@ -24,3 +24,9 @@ type ``Given a a for loop on an integer range`` () =
      ``when using an expression as the start & finish clause of the foor loop, it returns a Clojure for`` () =
         ConvertToClojure <@ let start = 1 in let finish = 9 in for i = (start - 1) to (finish + 1) do printfn "test" @>
         |> should equal "( def start  1  )\n( def finish  9  )\n(for [i ( range ( op_Subtraction  start  1 ) ( op_Addition ( op_Addition  finish  1 )  1 ) )] ( PrintFormatLine unknown) )"
+
+
+    [<Test>] member this.
+     ``when using an if expression as the start clause, it returns a Clojure for with an if in the start clause`` () =
+        ConvertToClojure <@ let start = 1 in let finish = 9 in for i = ( if start = 1 then 0 else 1) to (finish + 1) do printfn "test" @>
+        |> should equal "( def start  1  )\n( def finish  9  )\n(for [i ( range (if ( op_Equality  start  1 )  0  1 ) ( op_Addition ( op_Addition  finish  1 )  1 ) )] ( PrintFormatLine unknown) )"
